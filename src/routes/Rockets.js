566ca57c382578cff0a436ae4c2c3425759c074d
SFarida <faridagueleu@gmail.com>
2023-03-07 17:26:28 +0100

    Add rocket slice and basic page structure

--- src/routes/Rockets.js
+++ src/routes/Rockets.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Rockets = () => (
-  <div>rockets</div>
+  <section>
+    <div className="row">
+      <div className="col-md-2"><img width="200px" height="200px" src="images/planet.png" alt="rocket" /></div>
+      <div className="col-md-10">
+        <h4>Title</h4>
+        <p>Paragraph</p>
+        <button type="button" className="btn btn-primary">Reserve Rocket</button>
+      </div>
+    </div>
+  </section>
 );
 
 export default Rockets;
