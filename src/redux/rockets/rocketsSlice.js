6fbd4d0635241bf16c86257ceb8b22229da64d2f
SFarida <faridagueleu@gmail.com>
2023-03-07 23:10:44 +0100

    Add getRockets() to fetch rockets

--- src/redux/rockets/rocketsSlice.js
+++ src/redux/rockets/rocketsSlice.js
@@ -1,11 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [];
+const baseUrl = 'https://api.spacexdata.com/v4/rockets';
+
+const initialState = {
+  allRockets: [],
+  status: 'idle', // 'idle' | 'loading' | 'succeeded' | 'failed'
+  error: null,
+};
+
+export const getRockets = createAsyncThunk('rockets/fetchRockets', async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+});
 
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
-  reducers: {},
-});
+  reducers: {
 
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getRockets.pending, (state) => (
+        {
+          ...state,
+          status: 'loading',
+        }
+      ))
+      .addCase(getRockets.rejected, (state, action) => (
+        {
+          ...state,
+          status: 'failed',
+          error: action.error.message,
+        }
+      ))
+      .addCase(getRockets.fulfilled, (state, action) => (
+        {
+          ...state,
+          allRockets: action.payload,
+          status: 'succeeded',
+        }
+      ));
+  },
+});
+export const allRockets = (state) => state.rockets.allRocket;
+export const getRocketsStatus = (state) => state.rockets.status;
+export const getRocketsError = (state) => state.rockets.error;
 export default rocketsSlice.reducer;
--- src/routes/Rockets.js
+++ src/routes/Rockets.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getRockets } from '../redux/rockets/rocketsSlice';
 
-const Rockets = () => (
-  <section>
-    <div className="row">
-      <div className="col-md-2"><img width="200px" height="200px" src="images/planet.png" alt="rocket" /></div>
-      <div className="col-md-10">
-        <h4>Title</h4>
-        <p>Paragraph</p>
-        <button type="button" className="btn btn-primary">Reserve Rocket</button>
+const Rockets = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
+  return (
+    <section>
+      <div className="row">
+        <div className="col-md-2"><img width="200px" height="200px" src="images/planet.png" alt="rocket" /></div>
+        <div className="col-md-10">
+          <h4>Title</h4>
+          <p>Paragraph</p>
+          <button type="button" className="btn btn-primary">Reserve Rocket</button>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Rockets;
