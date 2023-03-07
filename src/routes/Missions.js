743a14cd4fd06397bbdfef930fa073099bda4e23
biruk-bereka <birukbeletech1212@gmail.com>
2023-03-07 20:55:49 +0300

    Create fetch missions function and dispatch on missions component

--- src/routes/Missions.js
+++ src/routes/Missions.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchMissions } from '../redux/missions/missionsSlice';
 
-const Missions = () => (
-  <table className="table">
-    <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Mission</th>
-        <th scope="col">Description</th>
-        <th scope="col">Status</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Missions</td>
-        <td>Description</td>
-        <td>Not A Member</td>
-      </tr>
-    </tbody>
-  </table>
-);
+const Missions = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchMissions());
+  }, []);
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Mission</th>
+          <th scope="col">Description</th>
+          <th scope="col">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Missions</td>
+          <td>Description</td>
+          <td>Not A Member</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
 
 export default Missions;
