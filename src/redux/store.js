38fa26330bfa693222064f790b5bb25120a99928
biruk-bereka <birukbeletech1212@gmail.com>
2023-03-07 22:18:11 +0300

    Display missions data on missions component as per design

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
 import missionsReducer from './missions/missionsSlice';
+
+const logger = createLogger({
+  collapsed: true,
+});
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+
 });
 
 export default store;
